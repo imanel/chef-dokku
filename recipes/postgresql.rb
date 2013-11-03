4eb606d852731dd9075015864b3aed8ddf26420c
Bernard Potocki <bernard.potocki@imanel.org>
2013-11-04 00:14:51 +0100

    Indentation

--- recipes/postgresql.rb
+++ recipes/postgresql.rb
@@ -1,9 +1,10 @@
 include_recipe "dokku"
 
 node[:dokku][:apps].keys.each do |app|
-execute "create postgresql database #{app}" do
-  command "dokku postgresql:create #{app}"
-  user    'dokku'
-  cwd     '/home/dokku'
-  not_if  "docker ps | grep postgresql/#{app}:latest"
+  execute "create postgresql database #{app}" do
+    command "dokku postgresql:create #{app}"
+    user    'dokku'
+    cwd     '/home/dokku'
+    not_if  "docker ps | grep postgresql/#{app}:latest"
+  end
 end
